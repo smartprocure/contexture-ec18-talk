7bac7175f5146d7bc77d6798f5e29bb6f2126f4e
Samuel Greene <sgreene@smartprocure.us>
2018-02-14 21:48:00 +0100

    CRA init

---
+++ talk/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
